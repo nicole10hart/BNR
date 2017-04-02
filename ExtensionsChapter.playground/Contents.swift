aa4bb4370478676c416d4344910cca04fb47df25
Nicole Hart <nicole.hart@capitalone.com>
2017-04-02 19:02:38 -0400

    Extensions chapter

---
+++ ExtensionsChapter.playground/Contents.swift
@@ -0,0 +1,82 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+typealias Velocity = Double
+
+extension Velocity {
+    var kph: Velocity { return self * 1.60934 }
+    var mph: Velocity { return self }
+}
+
+protocol Vehicle {
+    var topSpeed: Velocity { get }
+    var numberOfDoors: Int { get }
+    var hasFlatbed: Bool { get }
+}
+
+struct Car {
+    let make: String
+    let model: String
+    let year: Int
+    let color: String
+    let nickname: String
+    let numberOfDoors: Int
+    var gasLevel: Double {
+        willSet {
+            precondition(newValue <= 1.0 && newValue >= 0.0, "Newvalue must be between 0 and 1.")
+        }
+    }
+}
+
+extension Car: Vehicle {
+    var topSpeed: Velocity { return 180 }
+    //var numberOfDoors: Int { return 4 }
+    var hasFlatbed: Bool { return false }
+}
+
+extension Car {
+    init(make: String, model: String, year: Int, numberOfDoors: Int) {
+        self.init(make: make, model: model, year: year, color: "Black", nickname: "N/A", numberOfDoors: numberOfDoors, gasLevel: 1.0)
+    }
+}
+var c = Car(make: "Ford", model: "Fusion", year: 2013, numberOfDoors: 4)
+
+extension Car {
+    enum Kind {
+        case coupe, sedan
+    }
+    var kind: Kind {
+        if numberOfDoors == 2 {
+            return .coupe
+        }
+        else {
+            return .sedan
+        }
+    }
+}
+c.kind
+
+extension Car {
+    mutating func emptyGas(by amount: Double) {
+        precondition(amount <= 1 && amount > 0, "Amount to remove must be between 0 and 1.")
+        if (gasLevel-amount) >= 0.0 {
+            gasLevel -= amount
+        }
+        else {
+            gasLevel = 0.0
+        }
+        
+    }
+    
+    mutating func fillGas() {
+        gasLevel = 1.0
+    }
+}
+c.emptyGas(by: 0.3)
+c.gasLevel
+c.emptyGas(by: 0.8)
+c.gasLevel
+c.fillGas()
+c.gasLevel
+
